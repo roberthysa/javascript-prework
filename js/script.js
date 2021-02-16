169c450fd6cbf43784822ca143835cec9ba00a47
Robert Hysa <robert.hysa@wp.pl>
2021-02-16 19:48:26 +0100

    Add else-if to JS code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var playerMove;
-playerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Wygrałem!!!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
