667ce470eb2b9d1bb7bb82c23c8fc82d19b1118b
Robert Hysa <robert.hysa@wp.pl>
2021-02-16 18:54:01 +0100

    Add first JS code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+var playerMove;
+playerMove = 'kamień';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Wygrałem!!!');
