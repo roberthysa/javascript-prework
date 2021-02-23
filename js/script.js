753365876fbb92a482f0ee4ece4cd82df668bf1e
Robert Hysa <robert.hysa@wp.pl>
2021-02-23 22:13:56 +0100

    add fixes

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ const buttonScissors = document.getElementById('button-scissors');
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  let playerMove = argButtonName;
+  playerMove = argButtonName;
   console.log('ruch gracza to: ' + playerMove);
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
   displayResult(playerMove, computerMove);
 }
